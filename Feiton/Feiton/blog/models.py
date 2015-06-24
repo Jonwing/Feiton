d6bb5f52f0b29f2fe05ae7ed08d5e4307505c437
Jonwing <jonwing.lee@gmail.com>
2015-06-25 01:43:06 +0800

    elementary accomplish blog model, embed DjangoUeditor

--- Feiton/Feiton/blog/models.py
+++ Feiton/Feiton/blog/models.py
@@ -1,25 +1,57 @@
 from django.db import models
+from DjangoUeditor.models import UEditorField
+
+
+class Author(models.Model):
+    name = models.CharField(u"作者姓名", max_length=50)
+    email = models.EmailField(blank=True)
+    website = models.URLField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(u"标签", max_length=30)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Catagory(models.Model):
+    name = models.CharField(u"分类", max_length=50)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Artilces(models.Model):
-    caption = models.CharField(max_length=30)
-    subcaption = models.CharField(max_length=30)
+    caption = models.CharField(u"标题", max_length=30)
+    subcaption = models.CharField(u"副标题", max_length=30)
     publish_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author)
     catagory = models.ForeignKey(Catagory)
-    tags = models.ForeignKey(Tag, blank=True)
-    content = RichTextField()
-    abstract = RichTextField(blank=True)
+    tags = models.ManyToManyField(Tag)
+    content = UEditorField(
+        u"内容", width=600, height=300, toolbars="full",
+        imagePath="images/", filePath="add_on_files/",
+        upload_settings={"imageMaxSize": 1204000},
+        command=None, blank=True
+        )
+    abstract = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.caption
 
 
-class Author(models.Model):
-    pass
+class Statistics(models.Model):
+    article = models.OneToOneField(Artilces)
+    visits = models.IntegerField()
+    comments = models.IntegerField()
+    likes = models.IntegerField()
 
-
-class Tag(models.Model):
-    pass
-
-
-class Catagory(models.Model):
-    pass
+    def __unicode__(self):
+        return u"blog statistics"
--- Feiton/Feiton/settings.py
+++ Feiton/Feiton/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'DjangoUeditor',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -69,17 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Feiton.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Internationalization
