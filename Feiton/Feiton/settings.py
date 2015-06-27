62939bab69a9bc745b0d27eae8f3fcd4213525f0
Jonwing <jonwing.lee@gmail.com>
2015-06-28 01:35:17 +0800

    add duoshuo comments.

--- Feiton/Feiton/settings.py
+++ Feiton/Feiton/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'blog',
     'DjangoUeditor',
+    'duoshuo',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -97,6 +98,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+# duoshuo comments SDK settings
+DUOSHUO_SECRET = '77e5764bfb95da1179b4d65aa90b94a3'
+DUOSHUO_SHORT_NAME = 'Feiton'
 
 if DEBUG:
     from Feiton.config.feiton_cfg_dev import *
