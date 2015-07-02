a98234b0600f13a30e8b434f4695f1a2df7f336a
Jonwing <jonwing.lee@gmail.com>
2015-07-03 01:47:29 +0800

    fix media

--- Feiton/Feiton/settings.py
+++ Feiton/Feiton/settings.py
@@ -99,6 +99,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media/')
+MEDIA_URL = os.path.join(BASE_DIR, '/static/media/')
 # MEDIA_ROOT = '/static/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
