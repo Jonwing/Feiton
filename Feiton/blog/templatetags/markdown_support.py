699292dac16046b012b3252274bcd8338f4a4d1f
Jonwing <jonwing.lee@gmail.com>
2016-01-04 21:46:04 +0800

    换用MDL，支持markdown

--- Feiton/blog/templatetags/markdown_support.py
+++ Feiton/blog/templatetags/markdown_support.py
@@ -7,6 +7,8 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
+from blog.models import Article
 
 register = template.Library()
 
@@ -14,6 +16,16 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def markdown_support(content):
-    return mark_safe(markdown2.markdown(
-                                       force_text(content),
-                                       extras=['fenced-code-blocks', 'cuddled-lists', 'metadata', 'tables', 'spoiler']))
+    return mark_safe(
+        markdown2.markdown(
+            force_text(content),
+            extras=['fenced-code-blocks', 'cuddled-lists', 'metadata', 'tables', 'spoiler']))
+
+
+@register.filter(name="urlize")
+def get_id_and_slug(id):
+    try:
+        article = Article.objects.get(pk=id)
+        return reverse('article_detail', args=[article.id, article.slug])
+    except Article.DoesNotExist:
+        return '#'
